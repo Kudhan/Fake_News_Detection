21fd62f6a9dbd71e85dadde0e316937b8398088d
Kudhan <kudhanshaik01.com>
2025-04-04 21:07:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import React from 'react'
+import './components/Nav'
+import Nav from './components/Nav'
+function App() {
+  
+
+  return (
+    <>
+      console.log("hello");
+      <Nav />
+    </>
+  )
+}
+
+export default App
